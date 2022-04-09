f84b51bf4bdf7f7ade2a5bbd6c2160a44ecd561a
orkeilius <orkeilius@users.noreply.github.com>
2022-04-09 20:31:30 +0200

    refractor code

--- main.py
+++ main.py
@@ -109,10 +109,10 @@ async def pic(ctx, *arg):
 
 @bot.command()
 async def vid(ctx, *arg):
-    """manually take a video argument: time in second"""
+    """manually take a video (only in h264 because encoding on rasberry are slow) argument: time in second"""
     try:
         await ctx.send(
-            content="vid prise a {}".format(time.strftime("%Hh %Mmin %Ssec")),
+            content="video prise a {}".format(time.strftime("%Hh %Mmin %Ssec")),
             file=discord.File(take_video(int(arg[0]))),
         )
     except:
