1883cd283cd866cb294e561949312c279f4eed0a
orkeilius <orkeilius@users.noreply.github.com>
2022-04-10 10:28:01 +0200

    move video upload to another message

--- main.py
+++ main.py
@@ -110,10 +110,13 @@ async def pic(ctx, *arg):
 async def vid(ctx, *arg):
     """manually take a video (only in h264 because encoding on rasberry are slow) argument: time in second"""
     message = await ctx.send(content="enregistrement en cours...")
-    await message.edit(
-        content="video prise a {}".format(time.strftime("%Hh %Mmin %Ssec")),
-        file=discord.File(take_video(int(arg[0]))),
-    )
+    try:
+        await ctx.send(file=discord.File(take_video(int(arg[0]))))
+        await message.edit(
+            content="video prise a {}".format(time.strftime("%Hh %Mmin %Ssec")),
+        )
+    except:
+        await message.edit(content="", embed=makeEmbed(embedData["videoError"]))
 
 
 @bot.command()
